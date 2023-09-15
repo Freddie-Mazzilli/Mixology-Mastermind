f3d9814d5d3918bf3d50483b931c305007f86e16
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-09-15 19:28:44 -0400

    Sets up state for selected drink and form layout for ChangeDrink component

--- client/src/components/ChangeDrink.js
+++ client/src/components/ChangeDrink.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ChangeDrink() {
+function ChangeDrink({drinks, fetchDrinks}) {
+
+    const [selectedDrink, setSelectedDrink] = useState({
+        "id": "",
+        "name": "",
+        "image": "",
+        "instructions": "",
+        "ingredients": []
+    })
 
     return(
-        <>
-        <h2>Change Drink</h2>
-        </>
+        <div className="modify-drink-container">
+            <div className="add-drink-container">
+                <div className="form-flex">
+                    <h2>Selected Drink</h2>
+                    <form className="form-flex">
+                        <div className="form-flex2">
+                            <input className="drink-form" type="text" name="name" value="" placeholder="Drink Name" required/>
+                            <input className="drink-form" type="text" name="image" value="" placeholder="Image URL" required/>
+                            <textarea className="drink-form-ingredients" type="text" name="instructions" value="" placeholder="Instructions" required/>
+                            <textarea className="drink-form-ingredients" type="text" name="ingredients" value="" placeholder="Ingredients" required/>
+                        </div>
+                        <div className="form-flex2">
+                            <button className="form" type="submit">Add Drink</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
